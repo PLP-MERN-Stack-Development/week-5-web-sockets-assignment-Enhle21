4abc42fd27ea2720265aa85a357142cb100bc81e
Enhle21 <tshabangupalesa272@gmail.com>
2025-07-18 16:04:14 +0200

    Delete Chore.js

--- Chore.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const choreSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    assignedTo: { type: String },
-    dueDate: { type: Date },
-    isCompleted: { type: Boolean, default: false }
-}, { timestamps: true });
-export default mongoose.model('Chore', choreSchema);
