172798bb76a0f370d705ed57b093d05cab373e40
Enhle21 <tshabangupalesa272@gmail.com>
2025-07-18 16:05:25 +0200

    Delete chores.js

--- chores.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getChores, addChore, updateChore, deleteChore } from '../controllers/choreController.js';
-const router = express.Router();
-router.get('/', getChores);
-router.post('/', addChore);
-router.put('/:id', updateChore);
-router.delete('/:id', deleteChore);
-export default router;
